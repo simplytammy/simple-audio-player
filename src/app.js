a8688af59557a73130dcb3ebe895b4b1193e6ca3
simplytammy <ogurinkabenjamin@gmail.com>
2019-08-18 23:25:20 +0100

    Initial

---
+++ src/app.js
@@ -0,0 +1,73 @@
+function calculateTotalValue(length) {
+  var minutes = Math.floor(length / 60),
+    seconds_int = length - minutes * 60,
+    seconds_str = seconds_int.toString(),
+    seconds = seconds_str.substr(0, 2),
+    time = minutes + ':' + seconds
+
+  return time;
+}
+
+function calculateCurrentValue(currentTime) {
+  var current_hour = parseInt(currentTime / 3600) % 24,
+    current_minute = parseInt(currentTime / 60) % 60,
+    current_seconds_long = currentTime % 60,
+    current_seconds = current_seconds_long.toFixed(),
+    current_time = (current_minute < 10 ? "0" + current_minute : current_minute) + ":" + (current_seconds < 10 ? "0" + current_seconds : current_seconds);
+
+  return current_time;
+}
+
+function initProgressBar() {
+  var player = document.getElementById('player');
+  var length = player.duration
+  var current_time = player.currentTime;
+  var totalLength = calculateTotalValue(length)
+  jQuery(".end-time").html(totalLength);
+  var currentTime = calculateCurrentValue(current_time);
+  jQuery(".start-time").html(currentTime);
+
+  var progressbar = document.getElementById('seekObj');
+  progressbar.value = (player.currentTime / player.duration);
+  progressbar.addEventListener("click", seek);
+
+  if (player.currentTime == player.duration) {
+    $('#play-btn').removeClass('pause');
+  }
+
+  function seek(evt) {
+    var percent = evt.offsetX / this.offsetWidth;
+    player.currentTime = percent * player.duration;
+    progressbar.value = percent / 100;
+  }
+};
+
+function initPlayers(num) {
+  for (var i = 0; i < num; i++) {
+    (function() {
+      var playerContainer = document.getElementById('player-container'),
+        player = document.getElementById('player'),
+        isPlaying = false,
+        playBtn = document.getElementById('play-btn');
+      if (playBtn != null) {
+        playBtn.addEventListener('click', function() {
+          togglePlay()
+        });
+      }
+      function togglePlay() {
+        if (player.paused === false) {
+          player.pause();
+          isPlaying = false;
+          $('#play-btn').removeClass('pause');
+
+        } else {
+          player.play();
+          $('#play-btn').addClass('pause');
+          isPlaying = true;
+        }
+      }
+    }());
+  }
+}
+
+initPlayers(jQuery('#player-container').length);
